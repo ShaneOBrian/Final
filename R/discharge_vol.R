711432243f8eafb9faa144cb42bf2a78195c1d92
Shane O'Brian <shane@Shanes-MacBook-Pro.local>
2024-03-21 12:23:28 -0700

    finished package?

--- R/discharge_vol.R
+++ R/discharge_vol.R
@@ -4,9 +4,7 @@
 #' @param velocity rate of stream flow (feet per second)
 #' @return discharge (cubic feet per second)
 
-discharge_vol <- function(depth, velocity) {
-  width=2
-  result = depth*width*velocity
+discharge_vol <- function(depth, width = 2, velocity) {
+  result = depth * width * velocity
   return(result)
 }
-
